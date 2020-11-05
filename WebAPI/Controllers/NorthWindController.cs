aa8a9fef4f83cf3e23589400ddf7e2614230589d
Отважный Крылатый <andrey.paladin@gmail.com>
2020-11-05 16:20:48 +0200

    add files

---
+++ WebAPI/Controllers/NorthWindController.cs
@@ -0,0 +1,30 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    public class NorthWindController : ApiController
+    {
+        public IEnumerable<Employees> Get()
+        {
+            using (NorthwindEntities entities = new NorthwindEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+
+        public Employees Get(int empID)
+        {
+            using (NorthwindEntities entities = new NorthwindEntities())
+            {
+                return entities.Employees.FirstOrDefault(e => e.EmployeeID == empID);
+            }
+        }
+
+    }
+}
